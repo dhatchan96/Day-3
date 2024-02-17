3b885b8932665cdad81364a497085d542834ac02
dhatchan96 <dhatchan0@gmail.com>
2024-02-17 22:42:12 +0530

    added code

--- 1_a.js
+++ 1_a.js
@@ -3,4 +3,26 @@ Print odd numbers in an array */
 
 // Anonymous function
 
+let arr = [3,4,5,6,3,6,1,8,9,0];
 
+const oddNumbers = function (arr) {
+    for(i of arr) {
+        if(i % 2!== 0) {
+            console.log(i);
+        }
+    }
+}
+
+oddNumbers(arr);
+
+console.log('********************************');
+
+// IIFE
+
+(function (arr) {
+    for(i of arr) {
+        if(i % 2!== 0) {
+            console.log(i);
+        }
+    }
+})(arr);
